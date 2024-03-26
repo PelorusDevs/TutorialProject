d7fcfbc46d8cb75ce2a0630524248956ebdab846
Ayushj27 <ayush.j@pelorus.in>
2024-03-26 18:20:45 +0530

    Detailed nav changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
-      Git Tut
+      Git Tutorial
     </a>
     <button
       className="navbar-toggler"
@@ -28,43 +28,12 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">
-            Link
+            About
           </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">
-            Disabled
+          <a className="nav-link" href="#">
+            Contact us
           </a>
         </li>
       </ul>
